aa522953eb628faa80360231f3ecd55100a48d51
IskanderUraiymov <33644134+IskanderUraiymov@users.noreply.github.com>
2018-03-10 11:39:35 -0600

    Add files via upload

---
+++ A.java
@@ -0,0 +1,17 @@
+package warmup1;
+
+public class A {
+	int ii=5;
+	
+	public byte realM() {
+		System.out.println("Class A super");
+		return 1;
+	}
+	 void winningRM() {
+		 System.out.println("class A");
+	 }
+	 void lossingRM() {
+		 System.out.println("Class A");
+	 }
+
+}
