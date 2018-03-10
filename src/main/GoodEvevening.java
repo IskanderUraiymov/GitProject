8b6265fbe4fbff9c06eda76a3674ba133f70f3a3
iskanderuraiymov <iskanderuraiymov@10.1.10.99>
2018-03-10 11:21:27 -0600

    added print statement

--- src/main/GoodEvevening.java
+++ src/main/GoodEvevening.java
@@ -3,6 +3,8 @@ package main;
 public class GoodEvevening {
 	public static void main(String[] args) {
 		System.out.println("Good evevning class!");
+		
+		System.out.println("My Last Update");
 	}
 
 }
